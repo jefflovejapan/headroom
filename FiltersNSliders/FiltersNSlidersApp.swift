7c090b9f8eccba0d9667d7f432cea15416069f92
Jeff Blagdon <jeffblagdon@gmail.com>
2024-06-11 11:07:41 -0400

    Initial Commit

---
+++ FiltersNSliders/FiltersNSlidersApp.swift
@@ -0,0 +1,17 @@
+//
+//  FiltersNSlidersApp.swift
+//  FiltersNSliders
+//
+//  Created by Jeffrey Blagdon on 2024-06-11.
+//
+
+import SwiftUI
+
+@main
+struct FiltersNSlidersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
