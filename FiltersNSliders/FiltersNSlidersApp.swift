923c4fba4ec04a1fbe8df47daf08a9387f4c4bb3
Jeff Blagdon <jeffblagdon@gmail.com>
2024-06-11 12:50:48 -0400

    Little cleaner

--- FiltersNSliders/FiltersNSlidersApp.swift
+++ FiltersNSliders/FiltersNSlidersApp.swift
@@ -13,6 +13,6 @@ struct FiltersNSlidersApp: App {
         WindowGroup {
             ContentView()
         }
-        .environmentObject(ViewModel())
+        .environmentObject(try! ViewModel())
     }
 }
--- FiltersNSliders/ViewModel.swift
+++ FiltersNSliders/ViewModel.swift
@@ -18,18 +18,11 @@ class ViewModel: ObservableObject {
     @MainActor
     @Published var trackInfo: String?
     private(set) var bufferSize: UInt32 = 512
-    private let engine: AVAudioEngine
-    private let playerNode: AVAudioPlayerNode
-    private var audioFile: AVAudioFile?
     private var player: AudioPlayer?
+    private let engine: AudioEngine
     
-    init() {
-        let playerNode = AVAudioPlayerNode()
-        let engine = AVAudioEngine()
-        engine.attach(playerNode)
-        self.engine = engine
-        self.playerNode = playerNode
-        engine.connect(playerNode, to: engine.mainMixerNode, format: nil)
+    init() throws {
+        self.engine = AudioEngine()
     }
         
     @MainActor
@@ -43,32 +36,11 @@ class ViewModel: ObservableObject {
             throw Error.noFile(path)
         }
         
-        let engine = AudioEngine()
-        
         let audioFile: AVAudioFile = try AVAudioFile(forReading: url)
         let player = AudioPlayer(file: audioFile)!
         engine.output = player
-        try engine.start()
+        try self.engine.start()
         self.player = player
         player.play()
-        
-        
-        
-//        let audioSession = AVAudioSession.sharedInstance()
-//        try audioSession.setCategory(.playback, mode: .default, options: [])
-//        try audioSession.setActive(true)
-//        
-//        self.audioFile = audioFile
-        
-//        engine.outputNode.installTap(onBus: 0, bufferSize: 1024, format: audioFile.processingFormat) { buffer, time in
-//            print("Got a buffer: \(buffer) at time: \(time)")
-//        }
-
-        try engine.start()
-        await playerNode.scheduleFile(audioFile, at: nil)
-        playerNode.play()
-        
-        print("Player node started")
-
     }
 }
