5ae0dfc5500260f435b2b67acb3e6f1a627e0821
Jeff Blagdon <jeffblagdon@gmail.com>
2024-06-11 12:48:09 -0400

    Finally audio

--- FiltersNSliders/ViewModel.swift
+++ FiltersNSliders/ViewModel.swift
@@ -7,6 +7,7 @@
 
 import Foundation
 import AVFoundation
+import AudioKit
 
 class ViewModel: ObservableObject {
     enum Error: Swift.Error {
@@ -16,6 +17,20 @@ class ViewModel: ObservableObject {
     
     @MainActor
     @Published var trackInfo: String?
+    private(set) var bufferSize: UInt32 = 512
+    private let engine: AVAudioEngine
+    private let playerNode: AVAudioPlayerNode
+    private var audioFile: AVAudioFile?
+    private var player: AudioPlayer?
+    
+    init() {
+        let playerNode = AVAudioPlayerNode()
+        let engine = AVAudioEngine()
+        engine.attach(playerNode)
+        self.engine = engine
+        self.playerNode = playerNode
+        engine.connect(playerNode, to: engine.mainMixerNode, format: nil)
+    }
         
     @MainActor
     func load(path: String, pathExtension: String) async throws {
@@ -27,11 +42,33 @@ class ViewModel: ObservableObject {
             
             throw Error.noFile(path)
         }
-        let avAsset = AVAsset(url: url)
-            
-        guard let audioTrack = try await avAsset.loadTracks(withMediaType: .audio).first else {
-            throw Error.noAudioTracks
-        }
-        self.trackInfo = "\(try await audioTrack.load(.timeRange).duration.seconds) seconds of audio"
+        
+        let engine = AudioEngine()
+        
+        let audioFile: AVAudioFile = try AVAudioFile(forReading: url)
+        let player = AudioPlayer(file: audioFile)!
+        engine.output = player
+        try engine.start()
+        self.player = player
+        player.play()
+        
+        
+        
+//        let audioSession = AVAudioSession.sharedInstance()
+//        try audioSession.setCategory(.playback, mode: .default, options: [])
+//        try audioSession.setActive(true)
+//        
+//        self.audioFile = audioFile
+        
+//        engine.outputNode.installTap(onBus: 0, bufferSize: 1024, format: audioFile.processingFormat) { buffer, time in
+//            print("Got a buffer: \(buffer) at time: \(time)")
+//        }
+
+        try engine.start()
+        await playerNode.scheduleFile(audioFile, at: nil)
+        playerNode.play()
+        
+        print("Player node started")
+
     }
 }
